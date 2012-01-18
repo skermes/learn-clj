1768863cae185a3663548a7cef8c60fc9bc3927f
Sean Kermes <skermes@gmail.com>
2012-01-18 00:12:49 -0500

    Improving hash strategy testing.

--- kata/5.clj
+++ kata/5.clj
@@ -56,12 +56,14 @@
   (let [letters "abcdefghijklmnopqrstuvwxyz"]
     (string/join (map (fn [x] (rand-nth letters)) (range len)))))
 
+(def bad-word-list '("ywxqq" "mfqtf" "ldxbb" "ilccd" "gxehh" "clycx" "ctugb" "guqfh" "sryfs" "krkrb" "zituh" "iwcch" "nmuxd" "xywjo" "rbrep" "gyfkt" "qmscc" "xnmty" "yejix" "iesod" "yhhjv" "xpxfv" "zbhby" "nihaj" "wsohe" "xolhh" "axvua" "bepqp" "ebzdm" "gwymd" "zhfjg" "rijnb" "jfwzq" "xrbzw" "abuxb" "obptn" "rvdgt" "attqq" "drowp" "emtac" "bqclo" "ayafu" "mfgmd" "ntqaa" "ooxie" "djyhl" "qxtyp" "kjrel" "ypoph" "nnguv" "qwiqu" "rwbdy" "nusks" "zsuiz" "nxxgd" "iibro" "sedsq" "slces" "vusol" "gjhcy" "vgkhq" "apjhn" "dkjhw" "ghaaf" "blwla" "rbuhi" "egcsq" "kpyvf" "ttdku" "wrdjm" "tsyia" "ldyax" "zersx" "lloya" "slngl" "flgmt" "lhjix" "xishw" "knxrp" "afjpx" "ipnjb" "nlqbt" "kkbpd" "zdbkg" "otsds" "mgsry" "courc" "jwziq" "jvqnq" "twknh" "zmsam" "kuumr" "srsoj" "zvthv" "sahbj" "jmjjw" "sarot" "xwpyz" "ixmzd" "jkzar"))
+
 (defn get-results [is-a-word]
   (reduce (fn [results word] (if (is-a-word word)
                                  (assoc results :words (conj (results :words) word))
                                  (assoc results :failcount (+ 1 (results :failcount)))))
           (hash-map :words [] :failcount 0)
-          (map (fn [x] (random-word 5)) (range 100))))
+          bad-word-list))
 
 (defn test-hasher [strategy n-hashes hash-len]
   (do (println "Time for testing" n-hashes hash-len "-bit hashes from" strategy)
